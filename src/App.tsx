c9c269239fec6f762b8a57c80d9d4fe4ee3e97bb
baygut <berkaybaygut@gmail.com>
2024-11-27 03:36:40 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,24 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toaster";
 import Footer from "@/components/Footer";
-import { Etherspot, Home, Wagmi } from "@/pages";
+import { Etherspot, Home, Wagmi, PredictionMarket } from "@/pages";
 
 function App() {
-  return (
-    <Router>
-      <main className="min-h-screen flex flex-col justify-between">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/aa" element={<Etherspot />} />
-          <Route path="/wagmi" element={<Wagmi />} />
-        </Routes>
-        <Footer />
-        <Toaster />
-      </main>
-    </Router>
-  );
+    return (
+        <Router>
+            <main className="min-h-screen flex flex-col justify-between">
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/aa" element={<Etherspot />} />
+                    <Route path="/wagmi" element={<Wagmi />} />
+                    <Route path="/prediction" element={<PredictionMarket />} />
+                </Routes>
+                <Footer />
+                <Toaster />
+            </main>
+        </Router>
+    );
 }
 
 export default App;
